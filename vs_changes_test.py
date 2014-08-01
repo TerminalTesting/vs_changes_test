eb91b2e30512bfa13350b9f0b6adc1578567d8b4
TerminalTesting <testing@terminal.ru>
2014-08-01 13:08:00 +0600

    fix

--- vs_changes_test.py
+++ vs_changes_test.py
@@ -101,7 +101,7 @@ class VSChangesTest(unittest.TestCase):
         time.sleep(5)
         self.driver.find_element_by_class_name('addRsIb').click()
         time.sleep(20)
-        self.driver.execute_script(""$( '.input-collection' ).append( '<input style="display:none" checked="" name="ib[]" value="8d954cfc-0131-11e0-a3a6-0026188b0a94" type="checkbox">' )"")
+        self.driver.execute_script("""$( '.input-collection' ).append( '<input style="display:none" checked="" name="ib[]" value="8d954cfc-0131-11e0-a3a6-0026188b0a94" type="checkbox">' )""")
         self.driver.find_element_by_class_name('ui-dialog-buttonset').find_elements_by_tag_name('button')[0].click()
         self.driver.find_element_by_class_name('btn-primary').click()
         time.sleep(15)
