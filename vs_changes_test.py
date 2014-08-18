22cd7da5f469750490700becf5a629c9ad5e5ee2
TerminalTesting <testing@terminal.ru>
2014-08-18 18:16:40 +0600

    add fields check

--- vs_changes_test.py
+++ vs_changes_test.py
@@ -117,38 +117,162 @@ class VSChangesTest(unittest.TestCase):
             print u'Скриншот:\n' + self.ARTSOURCE + 'vsRsAddError.png'
             print
 
-        """ Удаление РС """
-        time.sleep(5)
-        self.driver.find_element_by_class_name('addRsIb').click()
-        time.sleep(10)
-        #self.driver.execute_script("""$( '.input-collection' ).empty()""")
-        self.driver.find_elements_by_class_name('dynatree-checkbox')[2].click()
-        time.sleep(5)
-        self.driver.find_element_by_class_name('ui-dialog-buttonset').find_elements_by_tag_name('button')[0].click()
-        time.sleep(15)
-        self.driver.find_element_by_class_name('btn-primary').click()
-        time.sleep(10)
-        try:
-            self.driver.find_element_by_class_name('adminVsNoChanged')
-            print u'Ошибка при удаление РС'
-            self.driver.get_screenshot_as_file('vsRsRemoveError.png')
-            print u'Скриншот:\n' + self.ARTSOURCE + 'vsRsRemoveError.png'
+        """ Изменение данных ВС """
+        #Общие настройки
+        self.driver.find_element_by_link_text(u'Общие настройки').click()
+        self.driver.find_element_by_id('categoryform_active').click()
+        self.driver.find_element_by_id('categoryform_name').clear()
+        self.driver.find_element_by_id('categoryform_name').send_keys('AutotestVSName')
+        self.driver.find_element_by_id('categoryform_alias').clear()
+        self.driver.find_element_by_id('categoryform_alias').send_keys('AutotestVSAlias')
+        self.driver.find_element_by_id('categoryform_menuColumnNumber').click()
+        self.driver.find_element_by_id('categoryform_menuColumnNumber').find_elements_by_tag_name('option')[1].click()
+        self.driver.execute_script("""$( '#categoryform_sort' ).attr('value', 777 )""")
+        self.driver.find_element_by_id('categoryform_description').send_keys('AutotestVSDescription')
+        self.driver.find_element_by_id('categoryform_picture').send_keys('AutotestVSPicture')
+        
+        self.driver.find_element_by_id('categoryform_flagBrand').click()
+        self.driver.find_element_by_id('categoryform_flagNew').click()
+        self.driver.find_element_by_id('categoryform_flagBrandLevel').click()
+        self.driver.find_element_by_id('categoryform_flagHideBrandLevel').click()
+        self.driver.find_element_by_id('categoryform_flagTermShow').click()
+        self.driver.find_element_by_id('categoryform_flagSetGroup').click()
+        self.driver.find_element_by_id('categoryform_showPromoBlock').click()
+        
+        self.driver.find_element_by_id('categoryform_externalLink').send_keys('AutotestVSLink')
+
+        #Служебные настройки
+        self.driver.find_element_by_link_text(u'Служебные настройки').click()
+        #self.driver.find_element_by_id('categoryform_oldId').send_keys('AutotestVSoldId')
+        #self.driver.find_element_by_id('categoryform_oldAlias').send_keys('AutotestVSoldAlias')
+
+        #SEO - настройки
+        self.driver.find_element_by_link_text(u'SEO - настройки').click()
+        self.driver.find_element_by_id('categoryform_metaTitle').send_keys('AutotestVSmetaTitle')
+        self.driver.find_element_by_id('categoryform_metaKeywords').send_keys('AutotestVSmetaKeywords')
+        self.driver.find_element_by_id('categoryform_metaDescription').send_keys('AutotestVSmetaDescription')
+        self.driver.find_element_by_id('categoryform_metaH1').send_keys('AutotestVSmetaH1')
+        self.driver.find_element_by_id('categoryform_singularName').send_keys('AutotestVSsingularName')
+        self.driver.find_element_by_id('categoryform_translitName').send_keys('AutotestVStranslitName')
+        
+        self.driver.find_element_by_class_name('btn-primary').click()#save changes
+
+        #Общие настройки
+        self.driver.find_element_by_link_text(u'Общие настройки').click()
+        if not self.driver.find_element_by_id('categoryform_active').is_selected():
             cnt += 1
+            print 'Флаг активности не снялся'
             print
-        except:
-            pass
+        if self.driver.find_element_by_id('categoryform_name').get_attribute('value') != 'AutotestVSName':
+            cnt += 1
+            print 'Текст categoryform_name не изменился - ', self.driver.find_element_by_id('categoryform_name').get_attribute('value')
+            print
+        if self.driver.find_element_by_id('categoryform_alias').get_attribute('value') != 'AutotestVSAlias':
+            cnt += 1
+            print 'Текст categoryform_alias не изменился - ', self.driver.find_element_by_id('categoryform_alias').get_attribute('value')
+            print
+        self.driver.find_element_by_id('categoryform_menuColumnNumber').click()
+        if self.driver.find_element_by_id('categoryform_menuColumnNumber').find_elements_by_tag_name('option')[1].get_attribute('selected') != 'selected':
+            cnt += 1
+            print 'Значение в categoryform_menuColumnNumber не изменилось - ', self.driver.find_element_by_id('categoryform_menuColumnNumber').find_elements_by_tag_name('option')[1].get_attribute('selected')
+            print
+        if self.driver.find_element_by_id('categoryform_sort').get_attribute('value') != '777':
+            cnt += 1
+            print 'Значение в categoryform_sort не изменилось - ', self.driver.find_element_by_id('categoryform_sort').get_attribute('value')
+            print
+        if self.driver.find_element_by_id('categoryform_description').get_attribute('value') != 'AutotestVSDescription':
+            cnt += 1
+            print 'Текст categoryform_description не изменился - ', self.driver.find_element_by_id('categoryform_description').get_attribute('value')
+            print
+        if self.driver.find_element_by_id('categoryform_picture').get_attribute('value') != 'AutotestVSPicture':
+            cnt += 1
+            print 'Текст categoryform_picture не изменился - ', self.driver.find_element_by_id('categoryform_picture').get_attribute('value')
+            print
+        
+        if not self.driver.find_element_by_id('categoryform_flagBrand').is_selected():
+            cnt += 1
+            print 'Флаг Brand не установился'
+            print
+        if not self.driver.find_element_by_id('categoryform_flagNew').is_selected():
+            cnt += 1
+            print 'Флаг New не установился'
+            print
+        if not self.driver.find_element_by_id('categoryform_flagBrandLevel').is_selected():
+            cnt += 1
+            print 'Флаг BrandLevel не установился'
+            print
+        if not self.driver.find_element_by_id('categoryform_flagHideBrandLevel').is_selected():
+            cnt += 1
+            print 'Флаг HideBrandLevel не установился'
+            print
+        if self.driver.find_element_by_id('categoryform_flagTermShow').is_selected():
+            cnt += 1
+            print 'Флаг TermShow не снялся'
+            print
+        if not self.driver.find_element_by_id('categoryform_flagSetGroup').is_selected():
+            cnt += 1
+            print 'Флаг SetGroup не установился'
+            print
+        if not self.driver.find_element_by_id('categoryform_showPromoBlock').is_selected():
+            cnt += 1
+            print 'Флаг PromoBlock не установился'
+            print
+        
+        if self.driver.find_element_by_id('categoryform_externalLink').get_attribute('value') != 'AutotestVSLink':
+            cnt += 1
+            print 'Текст externalLink не изменился - ', self.driver.find_element_by_id('categoryform_externalLink').get_attribute('value')
+            print
+
+        #Служебные настройки
+        self.driver.find_element_by_link_text(u'Служебные настройки').click()
+        #if self.driver.find_element_by_id('categoryform_oldId').get_attribute('value') != 'AutotestVSoldId':
+        #    cnt += 1
+        #    print 'Текст categoryform_oldId не изменился - ', self.driver.find_element_by_id('categoryform_oldId').get_attribute('value')
+        #    print
+        #if self.driver.find_element_by_id('categoryform_oldAlias').get_attribute('value') != 'AutotestVSoldAlias':
+        #    cnt += 1
+        #    print 'Текст categoryform_oldAlias не изменился - ', self.driver.find_element_by_id('categoryform_oldAlias').get_attribute('value')
+        #    print
+
+        #SEO - настройки
+        self.driver.find_element_by_link_text(u'SEO - настройки').click()
+        if self.driver.find_element_by_id('categoryform_metaTitle').get_attribute('value') != 'AutotestVSmetaTitle':
+            cnt += 1
+            print 'Текст categoryform_metaTitle не изменился - ', self.driver.find_element_by_id('categoryform_metaTitle').get_attribute('value')
+            print
+        if self.driver.find_element_by_id('categoryform_metaKeywords').get_attribute('value') != 'AutotestVSmetaKeywords':
+            cnt += 1
+            print 'Текст categoryform_metaKeywords не изменился - ', self.driver.find_element_by_id('categoryform_metaKeywords').get_attribute('value')
+            print
+        if self.driver.find_element_by_id('categoryform_metaDescription').get_attribute('value') != 'AutotestVSmetaDescription':
+            cnt += 1
+            print 'Текст categoryform_metaDescription не изменился - ', self.driver.find_element_by_id('categoryform_metaDescription').get_attribute('value')
+            print
+        if self.driver.find_element_by_id('categoryform_metaH1').get_attribute('value') != 'AutotestVSmetaH1':
+            cnt += 1
+            print 'Текст categoryform_metaH1 не изменился - ', self.driver.find_element_by_id('categoryform_metaH1').get_attribute('value')
+            print
+        if self.driver.find_element_by_id('categoryform_singularName').get_attribute('value') != 'AutotestVSsingularName':
+            cnt += 1
+            print 'Текст categoryform_singularName не изменился - ', self.driver.find_element_by_id('categoryform_singularName').get_attribute('value')
+            print
+        if self.driver.find_element_by_id('categoryform_translitName').get_attribute('value') != 'AutotestVStranslitName':
+            cnt += 1
+            print 'Текст categoryform_translitName не изменился - ', self.driver.find_element_by_id('categoryform_translitName').get_attribute('value')
+            print
+
+        
 
         """ Удаление ВС """
-        self.driver.find_element_by_link_text('AutotestVS').click()
+        self.driver.find_element_by_link_text('AutotestVSName').click()
         time.sleep(5)
         self.driver.find_element_by_class_name('delete').click()
-        conf_alert = self.driver.switch_to_alert()
-        conf_alert.accept()
+        self.driver.switch_to_alert().accept()
         time.sleep(10)
         li = self.driver.find_element_by_class_name('dynatree-container').find_elements_by_tag_name('li')
         vs_names = [x.find_element_by_tag_name('a').text for x in li]
 
-        if 'AutotestVS' in vs_names:
+        if 'AutotestVSName' in vs_names:
             cnt += 1
             print u'Ошибка при удалении ВС'
             self.driver.get_screenshot_as_file('vsRemoveError.png')
@@ -156,12 +280,3 @@ class VSChangesTest(unittest.TestCase):
             print
 
         assert cnt == 0, (u'Errors: %d' % cnt)
-
-
-
-
-
-
-
-
-        
