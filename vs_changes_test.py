0b1c2a6e5adb9a4386aaa82542bca8206a971b17
TerminalTesting <testing@terminal.ru>
2014-08-18 15:52:02 +0600

    try some changes

--- vs_changes_test.py
+++ vs_changes_test.py
@@ -100,9 +100,10 @@ class VSChangesTest(unittest.TestCase):
         
         time.sleep(5)
         self.driver.find_element_by_class_name('addRsIb').click()
-        time.sleep(20)
+        time.sleep(10)
         self.driver.execute_script("""$( '.input-collection' ).append( '<input style="display:none" checked="" name="ib[]" value="8d954cfc-0131-11e0-a3a6-0026188b0a94" type="checkbox">' )""")
         self.driver.find_element_by_class_name('ui-dialog-buttonset').find_elements_by_tag_name('button')[0].click()
+        time.sleep(5)
         self.driver.find_element_by_class_name('btn-primary').click()
         time.sleep(15)
         try:
@@ -117,9 +118,10 @@ class VSChangesTest(unittest.TestCase):
         """ Удаление РС """
         time.sleep(5)
         self.driver.find_element_by_class_name('addRsIb').click()
-        time.sleep(20)
+        time.sleep(10)
         self.driver.execute_script("""$( '.input-collection' ).empty()""")
         self.driver.find_element_by_class_name('ui-dialog-buttonset').find_elements_by_tag_name('button')[0].click()
+        time.sleep(5)
         self.driver.find_element_by_class_name('btn-primary').click()
         time.sleep(15)
         try:
