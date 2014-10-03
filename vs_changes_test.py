8081df47a376a4b90b6a739014ae4ac9a935e04c
TerminalTesting <testing@terminal.ru>
2014-10-03 14:11:20 +0600

    fix bug import error

--- vs_changes_test.py
+++ vs_changes_test.py
@@ -4,6 +4,9 @@ import unittest
 import sys
 import os, time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 #from sqlalchemy import create_engine, MetaData
 
 
